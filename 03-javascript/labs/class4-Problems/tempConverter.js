3bf789394efb838c7be894a3124d4dd65847cb03
Inesh-Reddy <ineshreddy007@gmail.com>
2026-07-13 08:11:05 +0530

    Problem: Temperature Converter

---
+++ 03-javascript/labs/class4-Problems/tempConverter.js
@@ -0,0 +1,27 @@
+/***
+ * Temperature converter
+ *
+ *  */
+
+const fahremheitToCelcius = (cel) => {
+  return Math.ceil((cel * 9) / 5 + 32);
+};
+const celciusToFahrenheit = (far) => {
+  return Math.ceil(((far - 32) * 5) / 9);
+};
+const convertTemperature = (temp, conversionUnit) => {
+  if (
+    typeof temp !== "number" ||
+    (conversionUnit !== "C" && conversionUnit !== "F")
+  ) {
+    return `Please enter valid input..`;
+  } else {
+    if (conversionUnit === "C") {
+      return celciusToFahrenheit(temp);
+    } else {
+      return fahremheitToCelcius(temp);
+    }
+  }
+};
+
+console.log(convertTemperature(100, "F"));
