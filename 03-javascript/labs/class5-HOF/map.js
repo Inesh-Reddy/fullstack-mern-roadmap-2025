c157605a19241815dca2118bc2800f3fe5cfde55
Inesh-Reddy <ineshreddy007@gmail.com>
2026-07-14 17:29:53 +0530

    Functional Programming : Map

--- 03-javascript/labs/class5-HOF/map.js
+++ 03-javascript/labs/class5-HOF/map.js
@@ -9,11 +9,20 @@ const nums = [1, 2, 3, 4, 5];
 // console.log(doubledNums);
 
 // Iteration - 2
-const doubleMe = (ele) => {
-  return ele * 2;
-};
+// const doubleMe = (ele) => {
+//   return ele * 2;
+// };
 
-const result = nums.map((ele) => {
-  return doubleMe(ele);
+// const result = nums.map((ele) => {
+//   return doubleMe(ele);
+// });
+// console.log(result);
+
+// given the radius, print out the areas
+const radius = [1, 2, 3, 4, 5];
+
+const area = radius.map((ele) => {
+  return Math.ceil(Math.PI * ele * ele);
 });
-console.log(result);
+
+console.log(area);
